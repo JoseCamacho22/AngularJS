faa1c83d8c279bbe148fb483918cf941521599fe
Jose Camacho <jacamacho@gfi.es>
2017-04-27 11:18:48 +0200

    Incluimos la directiva ng-repeat mostrando un listado

--- Practica3/js/app.js
+++ Practica3/js/app.js
@@ -4,7 +4,26 @@ var app = angular.module('universidadApp',[ ]);
 
 app.controller('listadoCtrl', ['$scope', function($scope){
 	
-		
+		$scope.listado =["Jose Camacho", "Sonia Garcia", "Yolanda Fernandez",]
+
+			$scope.ListadoProfesores={
+			profesores: [{
+				nombre:"Fernando Herrera",
+				edad:45,
+				clase:"Matematicas"
+			},
+			{
+				nombre:"Luis Herrera",
+				edad:35,
+				clase:"Systemas"
+			},
+			{
+				nombre:"Juan Lopez",
+				edad:50,
+				clase:"Programacion"
+			}]
+		}
+
 
 
 
