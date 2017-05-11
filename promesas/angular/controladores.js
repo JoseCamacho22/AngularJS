c0f812b98e94a625b190d4e85e259ffcffbea45a
Jose Camacho <jacamacho@gfi.es>
2017-05-11 14:21:02 +0200

    Comenzamos a trabajar con PROMESAS

---
+++ promesas/angular/controladores.js
@@ -0,0 +1,48 @@
+var app = angular.module('promesasApp.controladores', []);
+
+
+app.controller('mainCtrl', ['$scope','$q',function($scope,$q){
+
+
+	$scope.miVar = 0;
+
+	$scope.sumar = function (num){
+
+		var q = $q.defer();
+		var valido = true;
+
+		num ++;
+
+	setTimeout(function(){
+
+		if (valido) {
+
+				q.resolve( num )
+		}else{
+
+			q.reject( 'No se suma ');
+		}
+
+		},3000);
+
+		return q.promise;
+	}
+
+
+	$scope.promise  = $scope.sumar( 1);
+	
+	$scope.promise.then(
+	function(valor){
+
+		console.log("Promesa cumplida");
+		$scope.miVar= valor;
+
+	} ,
+	 function(error){
+
+	 	console.error (error);
+	 	$scope.miVar = "Errorrrrrrrr !!!!!!"
+	});
+
+
+}])
