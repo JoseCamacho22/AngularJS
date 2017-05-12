2940ad1359e9b7d34bb131bc73d7adad241dfe41
Jose Camacho <jacamacho@gfi.es>
2017-05-12 13:46:16 +0200

    Paginacion del lado del Servidor

---
+++ paginacion/angular/controladores.js
@@ -0,0 +1,36 @@
+var app = angular.module('paginacionApp.controladores',[]);
+
+app.controller('paisesCtrl', ['$scope','Paises', function($scope,Paises){
+		
+	
+	$scope.paises = Paises;
+
+	$scope.irUltimo= function(){
+
+		Paises.cargarUltima();
+
+	}
+
+	$scope.irPrimera = function (){
+		Paises.cargarPrimera();
+	}
+
+	$scope.irA = function ( num ) {
+		Paises.irA( num );
+		// body...
+	}
+
+	$scope.arrPaginas = function ( num ) {
+
+		var arr = [];
+
+		for ( var i= 0; i< num; i++ ){
+			arr.push( i+1);
+		}
+
+		return arr;
+
+	}
+
+
+}]);
